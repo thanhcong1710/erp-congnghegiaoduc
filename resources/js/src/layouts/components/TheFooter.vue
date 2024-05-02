415cf62a4e55717f0c8be0aac6ca23fbed4851e1
conglt <thanhcong1710@gmail.com>
2024-05-02 16:58:44 +0700

    update

--- resources/js/src/layouts/components/TheFooter.vue
+++ resources/js/src/layouts/components/TheFooter.vue
@@ -14,13 +14,13 @@
         <p>
           <span>COPYRIGHT &copy;</span>
           <span>{{ new Date().getFullYear() }} </span>
-          <a href="https://1.envato.market/vuexy_admin" target="_blank" rel="nofollow">Pixinvent</a>
+          <a href="#" target="_blank" rel="nofollow">Công nghệ giáo dục</a>
           <span class="hidden sm:inline-block">, All rights Reserved</span>
         </p>
-        <span class="md:flex hidden items-center">
+        <!-- <span class="md:flex hidden items-center">
             <span>Hand-crafted & Made with</span>
             <feather-icon icon="HeartIcon" svgClasses="stroke-current text-danger w-6 h-6" class="ml-2" />
-        </span>
+        </span> -->
 
         <!-- buyNow component -->
         <!-- <component :is="injections.components.BuyNow"></component> -->
