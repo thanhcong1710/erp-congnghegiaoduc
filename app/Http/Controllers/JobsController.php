f72abbede593c52a2977f60c333b78f77c3dcc43
Cong Luong <conglt@9pay.vn>
2025-02-28 10:21:02 +0700

    update

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -34,7 +34,7 @@ class JobsController extends Controller
 
     public function processAutoWithdrawStudent()
     {
-        $list = u::query("SELECT id FROM contracts WHERE status!=7 AND left_sessions =0 AND summary_sessions > 0'");
+        $list = u::query("SELECT id FROM contracts WHERE status!=7 AND left_sessions =0 AND summary_sessions > 0");
         foreach ($list AS $row) {
             u::updateSimpleRow(array(
                 'status' => 7,
