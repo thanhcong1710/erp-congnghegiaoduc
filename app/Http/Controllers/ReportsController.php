06c73a7a8cab5fb18f4d4b5ed96c034c606281d6
thanhcong1710 <thanhcong1710@gmail.com>
2025-08-05 14:21:26 +0700

    update

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -56,7 +56,7 @@ class ReportsController extends Controller
                 AND c.enrolment_start_date <= ( SELECT class_date FROM schedules WHERE class_id = c.class_id AND class_date >= '$end_date' AND `status`=1 ORDER BY class_date ASC LIMIT 1 )
                 AND c.enrolment_last_date >= ( SELECT class_date FROM schedules WHERE class_id = c.class_id AND class_date <= '$end_date' AND `status`=1 ORDER BY class_date ASC LIMIT 1 )
             )
-            AND (SELECT count(id) FROM reserves WHERE contract_id=c.id AND is_reserved=1 AND `start_date` <= '$end_date' AND `end_date`>='$end_date' AND `status`=2) =0
+            AND (SELECT count(id) FROM reserves WHERE contract_id=c.id AND is_reserved=1 AND `start_date` <= '$end_date' AND `end_date`>='$end_date' AND `status`=4) =0
             AND s.status > 0
         $where ");
         if (count($list)) {
@@ -98,36 +98,72 @@ class ReportsController extends Controller
         $limit = isset($pagination->limit) ? (int) $pagination->limit : 20;
         $offset = $page == 1 ? 0 : $limit * ($page-1);
         $limitation =  $limit > 0 ? " LIMIT $offset, $limit": "";
-        $cond = " r.branch_id IN (" . Auth::user()->getBranchesHasUser().")";
 
-        if (!empty($branch_id)) {
-            $cond .= " AND r.branch_id IN (".implode(",",$branch_id).")";
-        }
-        
-        if ($keyword !== '') {
-            $cond .= " AND (s.lms_code LIKE '%$keyword%' OR s.name LIKE '%$keyword%') ";
-        }
+        if ($start_date == date('Y-m') && 1==2) {
+            $end_date = date('Y-m-d');
+            $cond = " c.branch_id IN (" . Auth::user()->getBranchesHasUser().")";
+            $order_by = " ORDER BY c.id DESC ";
+            $total = u::first("SELECT count(r.id) AS total 
+                FROM report_full_fee_active AS r LEFT JOIN students AS s ON s.id=r.student_id WHERE $cond");
 
-        if ($start_date !== '') {
-            $cond .= " AND r.report_month = '$start_date'";
+            $list = u::query("SELECT DISTINCT s.id student_id,
+                c.id contract_id,c.tuition_fee_id, c.init_tuition_fee_id,
+                s.branch_id,
+                IF((c.class_id !=0 AND c.class_id IS NOT NULL),c.class_id,(SELECT class_id FROM contracts WHERE student_id=s.id AND `status` !=7 AND `class_id` IS NOT NULL LIMIT 1)) AS class_id,
+                t.cm_id,
+                c.product_id,
+                '$report_month' report_month,
+                c.enrolment_last_date,
+                c.summary_sessions, c.done_sessions,c.enrolment_start_date, c.last_done_sessions,
+                IF(c.count_recharge =0 , 0, 1) AS type_fee
+            FROM
+                contracts c
+                LEFT JOIN students s ON c.student_id = s.id
+                LEFT JOIN classes cls ON c.class_id = cls.id
+                LEFT JOIN term_student_user AS t ON t.student_id = c.student_id
+                LEFT JOIN users u ON t.cm_id = u.id
+            WHERE
+                c.type > 0
+                AND c.`status` < 7
+                AND (
+                    c.class_id IS NOT NULL
+                    AND c.enrolment_start_date <= ( SELECT class_date FROM schedules WHERE class_id = c.class_id AND class_date >= '$end_date' AND `status`=1 ORDER BY class_date ASC LIMIT 1 )
+                    AND c.enrolment_last_date >= ( SELECT class_date FROM schedules WHERE class_id = c.class_id AND class_date <= '$end_date' AND `status`=1 ORDER BY class_date ASC LIMIT 1 )
+                )
+                AND (SELECT count(id) FROM reserves WHERE contract_id=c.id AND is_reserved=1 AND `start_date` <= '$end_date' AND `end_date`>='$end_date' AND `status`=4) =0
+                AND s.status > 0
+            $cond $order_by $limitation");
+        } else {
+            $cond = " r.branch_id IN (" . Auth::user()->getBranchesHasUser().")";
+            if (!empty($branch_id)) {
+                $cond .= " AND r.branch_id IN (".implode(",",$branch_id).")";
+            }
+            
+            if ($keyword !== '') {
+                $cond .= " AND (s.lms_code LIKE '%$keyword%' OR s.name LIKE '%$keyword%') ";
+            }
+    
+            if ($start_date !== '') {
+                $cond .= " AND r.report_month = '$start_date'";
+            }
+            
+            $order_by = " ORDER BY r.id DESC ";
+    
+            $total = u::first("SELECT count(r.id) AS total 
+                FROM report_full_fee_active AS r LEFT JOIN students AS s ON s.id=r.student_id WHERE $cond");
+            
+            $list = u::query("SELECT b.name AS branch_name, s.lms_code, s.name, s.gud_name1, cl.cls_name, p.name AS product_name,
+                    CONCAT (u.hrm_id, ' - ', u.name) AS cm_name, t.name AS tuition_fee_name,
+                    IF(r.type=0, 'NEW', 'RENEW') AS type_fee, r.last_done_sessions, r.done_sessions,r.summary_sessions,r.start_date, r.end_date
+                FROM report_full_fee_active AS r 
+                    LEFT JOIN students AS s ON s.id=r.student_id
+                    LEFT JOIN branches AS b ON b.id = r.branch_id
+                    LEFT JOIN classes AS cl ON cl.id = r.class_id
+                    LEFT JOIN products AS p ON p.id = r.product_id
+                    LEFT JOIN tuition_fee AS t ON t.id = r.init_tuition_fee_id
+                    LEFT JOIN users AS u ON u.id=r.cm_id
+                WHERE $cond $order_by $limitation");
         }
-        
-        $order_by = " ORDER BY r.id DESC ";
-
-        $total = u::first("SELECT count(r.id) AS total 
-            FROM report_full_fee_active AS r LEFT JOIN students AS s ON s.id=r.student_id WHERE $cond");
-        
-        $list = u::query("SELECT b.name AS branch_name, s.lms_code, s.name, s.gud_name1, cl.cls_name, p.name AS product_name,
-                CONCAT (u.hrm_id, ' - ', u.name) AS cm_name, t.name AS tuition_fee_name,
-                IF(r.type=0, 'NEW', 'RENEW') AS type_fee, r.last_done_sessions, r.done_sessions,r.summary_sessions,r.start_date, r.end_date
-            FROM report_full_fee_active AS r 
-                LEFT JOIN students AS s ON s.id=r.student_id
-                LEFT JOIN branches AS b ON b.id = r.branch_id
-                LEFT JOIN classes AS cl ON cl.id = r.class_id
-                LEFT JOIN products AS p ON p.id = r.product_id
-                LEFT JOIN tuition_fee AS t ON t.id = r.init_tuition_fee_id
-                LEFT JOIN users AS u ON u.id=r.cm_id
-            WHERE $cond $order_by $limitation");
 
         $data = u::makingPagination($list, $total->total, $page, $limit);
         return response()->json($data);
