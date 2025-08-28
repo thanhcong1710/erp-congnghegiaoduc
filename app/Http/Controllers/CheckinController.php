9bd6b1c83840e767b166686e631d782b0c9b6516
thanhcong1710 <thanhcong1710@gmail.com>
2025-08-28 15:22:19 +0700

    update

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -144,6 +144,7 @@ class CheckinController extends Controller
             ), 'term_student_user');
 
             u::updateSimpleRow(array('status'=>3, 'lms_id' =>$lms_student_id), array('id'=> data_get($crm_student_info, 'id')), 'crm_students');
+            u::updateSimpleRow(array('status'=>3), array('crm_student_id'=> data_get($crm_student_info, 'id')), 'crm_student_checkin');
 
             $last_lms_code = str_pad((string)$lms_student_id, 6, '0', STR_PAD_LEFT);
             $lms_code = config('app.prefix_student_code').$last_lms_code;
