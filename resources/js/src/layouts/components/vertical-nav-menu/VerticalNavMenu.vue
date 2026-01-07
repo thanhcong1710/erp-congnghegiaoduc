ed4b6183e6b0fb71062180f5991fd51a2d0baf7e
thanhcong1710 <thanhcong1710@gmail.com>
2026-01-07 16:56:12 +0700

    update

--- resources/js/src/layouts/components/vertical-nav-menu/VerticalNavMenu.vue
+++ resources/js/src/layouts/components/vertical-nav-menu/VerticalNavMenu.vue
@@ -33,7 +33,8 @@
           <router-link tag="div" class="vx-logo cursor-pointer flex items-center" to="/">
             <!-- <logo class="w-10 mr-4 fill-current text-primary" />
             <span class="vx-logo-text text-primary" v-show="isMouseEnter || !reduce" v-if="title">{{ title }}</span> -->
-            <img src="@assets/images/logo/logo.png" alt="Logo" style="height: 56px;">
+            <img src="@assets/images/logo/logo.png" alt="Logo" style="height: 56px; margin-right: 10px;">
+            <h2 class="brand-text" style="color: #014CCC; font-family: serif; font-weight: normal; letter-spacing: 0.01rem; font-size: 36px;"> LAPO </h2>
           </router-link>
           <!-- /Logo -->
 
