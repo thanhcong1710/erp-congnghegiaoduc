435ef35b5d304bd616be5750989c274d0cd95a97
thanhcong1710 <thanhcong1710@gmail.com>
2026-07-28 11:13:42 +0700

    update

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -44,31 +44,10 @@ class Test extends Command
      */
     public function handle(Request $request)
     {
-        // $list = u::query("SELECT c.* FROM contracts As c LEFT JOIN classes AS cl ON cl.id=c.class_id WHERe c.status=6 AND cl.cls_enddate > CURRENT_DATE AND c.debt_amount=0 AND c.id >9912");
-        // $list = u::query("SELECT id,enrolment_start_date FROM contracts WHERE enrolment_start_date < '2026-05-07'  AND `status`=6 AND debt_amount=0");
-        // foreach($list AS $row){
-        //     u::processDataContractsPast(data_get($row, 'id'),data_get($row, 'enrolment_start_date'));
-
-        //     echo data_get($row, 'id')."/";
-        // }
-        $charge = new ChargesController();
-        // $list = u::query("SELECT DISTINCT agreement_id FROM payments WHERe is_import=2");
-        // foreach($list AS $row){
-        //     $charge->reProcessAgreement($row->agreement_id);
-        //     echo  $row->agreement_id."/";
-        // }
-        foreach([207,
-        597,
-        4153,
-        6317,
-        7053,
-        9132,
-        9635,
-        9638,
-        10007] as $row){
-
-        $charge->reProcessAgreement($row);
-         echo  $row."/";
+        $list= u::query("SELECT * FROM contracts WHERE class_id IS NOT NULL AND status !=7");
+        foreach ($list AS $row){
+            u::updateScheduleHasStudent($row->id,$row->enrolment_start_date);
+            echo $row->id."/";
         }
         
         return "ok";
