d1a0fa8445ecaa793f3cf2d6f682ab78de7e7b15
thanhcong1710 <thanhcong1710@gmail.com>
2026-07-29 15:10:57 +0700

    update

--- resources/js/src/views/lms/report/operate/refundDeposit.vue
+++ resources/js/src/views/lms/report/operate/refundDeposit.vue
@@ -80,8 +80,6 @@
 
 <script>
 import DatePicker from "vue2-datepicker";
-import "vue2-datepicker/index.css";
-import "vue2-datepicker/locale/vi";
 import axios from "@/http/axios.js";
 
 export default {
