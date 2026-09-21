58e500a6188c6aab3911bdd5e0aef1788e8f6b8f
thanhcong1710 <thanhcong1710@gmail.com>
2026-09-21 17:07:53 +0700

    update

--- resources/js/src/views/lms/report/operate/report27.vue
+++ resources/js/src/views/lms/report/operate/report27.vue
@@ -110,7 +110,7 @@
                 <div class="flex items-center gap-2" v-if="item.book_receive == 1">
                   <date-picker :disabled="user_role.is_sale || user_role.is_sale_leader" style="width:140px" v-model="item.book_delivered_date" type="date" format="YYYY-MM-DD" :lang="datepickerOptions.lang" placeholder="Chọn ngày" @change="updateSingleDate(item)"></date-picker>
                 </div>
-                <div class="flex items-center gap-2" v-if="item.book_receive == 3">
+                <div class="flex items-center gap-2" v-else="item.book_receive != 2 ">
                   {{ fmtDate(item.book_delivered_date) }}
                 </div>
               </td>
